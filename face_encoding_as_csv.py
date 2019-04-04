9a7d4d19f6b1800d312ef9824243b390b7aa34a7
Maxime Moreillon <m.moreillon@gmail.com>
2019-04-04 16:07:16 +0900

    v1 working

--- face_encoding_as_csv.py
+++ face_encoding_as_csv.py
@@ -1,1 +1,27 @@
 import face_recognition
+import os
+import numpy as np
+INPUT_DIR_PATH = "./input"
+OUTPUT_DIR_PATH = "./output"
+
+# Fill an array with paths of all images of a folder
+image_paths = []
+for image in os.listdir(INPUT_DIR_PATH):
+    if image.endswith(".jpg") or image.endswith(".png"):
+        image_paths.append(os.path.join(INPUT_DIR_PATH,image))
+
+
+for image_path in image_paths:
+    # Load the image
+    image = face_recognition.load_image_file(image_path)
+    # Compute the face encodings
+    # WHY [0]?
+    face_encoding = face_recognition.face_encodings(image)[0]
+
+    image_name = os.path.splitext(os.path.basename(image_path))[0]
+
+    np.savetxt(
+        os.path.join(OUTPUT_DIR_PATH,image_name+".csv"),
+        face_encoding,
+        fmt='%10.20f'
+        )
